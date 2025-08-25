a5b5b8b193c94ea8dfeea36ce93aa9ee9020fa6e
Andre Schenato <andreschenato@gmail.com>
2025-08-25 20:38:22 -0300

    Separate main component from app component

--- lib/main.dart
+++ lib/main.dart
@@ -1,63 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:prism/core/theme/app_theme.dart';
-import 'package:prism/core/widgets/menu.dart';
+import 'package:prism/app.dart';
 
 void main() {
   runApp(const PrismApp());
-}
-
-class PrismApp extends StatelessWidget {
-  const PrismApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Prism',
-      theme: AppTheme.lightTheme,
-      home: const MyHomePage(title: 'Prism'),
-      debugShowCheckedModeBanner: false,
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _selectedIndex = 0;
-
-  static final List<Widget> _widgetOptions = <Widget>[
-    const Center(child: Text('Home Page', style: TextStyle(fontSize: 24))),
-
-    const Center(child: Text('Favorites Page', style: TextStyle(fontSize: 24))),
-
-    const Center(child: Text('Profile Page', style: TextStyle(fontSize: 24))),
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: IndexedStack(index: _selectedIndex, children: _widgetOptions),
-      bottomNavigationBar: Menu(
-        currentIndex: _selectedIndex,
-        onTap: _onItemTapped,
-      ),
-    );
-  }
 }
