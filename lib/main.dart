d877983f0c285371687461d9fdf9f08dae6be5be
Andre Schenato <andreschenato@gmail.com>
2025-09-02 19:41:16 -0300

    chore: add firebase dependencies and make initial configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:prism/app.dart';
+import 'package:prism/core/config/firebase_options.dart';
 import 'package:prism/core/config/locator.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   setupLocator();
   runApp(const ProviderScope(child: PrismApp()));
 }
