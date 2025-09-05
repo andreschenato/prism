ebe197fbf1e54e7524292843d01b81d6f700ee2c
Andre Schenato <andreschenato@gmail.com>
2025-09-05 19:28:23 -0300

    chore: changed firebase_options default path

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:prism/app.dart';
-import 'package:prism/core/config/firebase_options.dart';
+import 'package:prism/firebase_options.dart';
 import 'package:prism/core/config/locator.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:firebase_core/firebase_core.dart';
