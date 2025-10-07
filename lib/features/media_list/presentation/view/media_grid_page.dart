4c8d2717efa877beaf78c37f061db391c0d6e01d
henri.grzegozeski <104114@aluno.uricer.edu.br>
2025-10-07 11:14:46 -0300

    SearchBar widget created

--- lib/features/media_list/presentation/view/media_grid_page.dart
+++ lib/features/media_list/presentation/view/media_grid_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:prism/core/widgets/list_builder.dart';
 import 'package:prism/core/widgets/media_card.dart';
+import 'package:prism/core/widgets/search_bar.dart';
 import 'package:prism/features/media_list/domain/entities/media_entity.dart';
 
 class MediaGridPage extends StatelessWidget {
@@ -18,22 +19,37 @@ class MediaGridPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text(title)),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-        child: ListBuilder(
-          itemBuilder: (context, index) {
-            final item = media[index];
-            return MediaCard(
-              label: item.title,
-              onPressed: () => context.go('/media/${item.id}'),
-              iconPlaceholder: Icons.movie_creation_rounded,
-              imageUrl: item.posterUrl,
-            );
-          },
-          itemCount: media.length,
-          axisCount: 3,
-          contentHeight: 210,
-        ),
+      body: Column(
+        children: [
+          SizedBox(
+            height: 80,
+            child: SearchBarApp(
+              hintText: 'Search $title',
+              onChanged: (value) {
+                // TODO: Implement search functionality
+              },
+            ),
+          ),    
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              child: ListBuilder(
+                itemBuilder: (context, index) {
+                  final item = media[index];
+                  return MediaCard(
+                    label: item.title,
+                    onPressed: () => context.go('/media/${item.id}'),
+                    iconPlaceholder: Icons.movie_creation_rounded,
+                    imageUrl: item.posterUrl,
+                  );
+                },
+                itemCount: media.length,
+                axisCount: 3,
+                contentHeight: 210,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
